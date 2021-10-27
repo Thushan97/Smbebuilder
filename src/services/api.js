91396314ee81c69d6dcbdba9598f24c48525b2bd
Dushan Pramod <dushanpramod@gmail.com>
2021-10-28 04:00:44 +0530

    register and login completed

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from "axios";
+
+const getInstance = () => {
+  const instance = axios.create({
+    baseURL: `${process.env.REACT_APP_API_HOST}/api`,
+    withCredentials: true,
+    timeout: 30000,
+  });
+  return instance;
+};
+
+export default {
+  auth: {
+    register: (data) => getInstance().post("auth/register", data),
+    login: (data) => getInstance().post("auth/login", data),
+    logout: () => getInstance().get("auth/logout"),
+    forgotPassword: (data) => getInstance().post("auth/forgotPassword", data),
+    resetPassword: (data) => getInstance().post("auth/resetPassword", data),
+    getUser: () => getInstance().get("auth/user"),
+  },
+};
